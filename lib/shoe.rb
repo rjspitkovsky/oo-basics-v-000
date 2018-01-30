f665a2b33c276988774d193d94c59649309da509
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-30 19:11:55 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,16 @@
 class Shoe
+attr_accessor :color, :size, :material, :condition
+attr_reader :brand
+
 
   def initialize(brand)
     @brand = brand
   end
 
+
+  def cobble
+    puts "the shoe has been repaired!"
+    condition = "new"
+  end 
 
 end
