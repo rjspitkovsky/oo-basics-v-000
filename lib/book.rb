162db1b7737d943d881401b1b6d7706773e705c2
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-30 19:02:36 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,18 @@
 class Book
   def initialize(title)
     @title = title
+  end
 
 def title
-  @title 
+  @title
 end
 
+def author_name=(author_name)
+  @author_name = author_name
 end
+
+def author_name
+  @author_name
+end 
+
 end
